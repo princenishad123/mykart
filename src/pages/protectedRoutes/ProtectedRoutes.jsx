8d014ca448dd80a68c055e61a929b29e830d332b
pricnenishad904 <princenishad904@gmail.com>
2024-03-11 12:49:11 +0530

    solve nelify refresh error

--- src/pages/protectedRoutes/ProtectedRoutes.jsx
+++ src/pages/protectedRoutes/ProtectedRoutes.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 const ProtectedRoutes = ({ Component }) => {
   const isLoggedIn = localStorage.getItem("user");
   const navigate = useNavigate();
+
   useEffect(() => {
     if (!isLoggedIn) {
       navigate("/");
