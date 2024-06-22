e9e48e9a7ad70651a2366dcac0bacc74737524d9
Prince Nishad <pn3147399@gmail.com>
2024-06-22 22:11:35 +0530

    update refund policy

--- src/pages/RefundAndCans.jsx
+++ src/pages/RefundAndCans.jsx
@@ -4,7 +4,80 @@ import Layout from '../components/layout/Layout'
 const RefundAndCans = () => {
   return (
 <Layout>
-/refund-and-cancellation
+<div className='w-[70%] mx-auto max-md:w-[95%]'>
+
+    
+
+<h1 className='text-2xl font-bold'>Refund and Cancellation</h1>
+<br />
+<br />
+At eShopsy, we strive to provide our customers with the best shopping experience possible. We understand that sometimes you may need to return or cancel an order. This policy outlines the terms and conditions for refunds and cancellations.
+
+<br />
+<br />
+<h1 className='text-2xl font-semibold'>Eligibility for Refunds</h1>
+
+We accept returns for most products within 30 days of purchase. To be eligible for a refund, the item must be:
+
+Unused and in the same condition that you received it.
+In its original packaging.
+Accompanied by the original receipt or proof of purchase.
+Non-Returnable Items
+Certain items are not eligible for returns or refunds, including:
+
+Perishable goods (e.g., food, flowers, newspapers, or magazines)
+Customized or personalized items
+Downloadable software products
+Gift cards
+Health and personal care items
+Return Process
+To initiate a return, please follow these steps:
+
+Contact Customer Service: Email us at support@eshopsy.netlify.app with your order number and reason for return.
+Receive Return Authorization: Wait for our customer service team to provide you with a return authorization and instructions on where to send your item.
+Ship the Item: Pack the item securely in its original packaging and include all original documentation. Ship the item to the address provided by our customer service team.
+<br />
+<br />
+<h1 className='text-2xl font-semibold'>Refund Processing</h1>
+
+Once your return is received and inspected, we will notify you of the approval or rejection of your refund. If approved, your refund will be processed, and a credit will automatically be applied to your original method of payment within a certain number of days. Please note that shipping costs are non-refundable.
+<br />
+<br />
+<h1 className='text-2xl font-semibold'>Late or Missing Refunds</h1>
+
+
+If you haven’t received a refund yet, please check your bank account again. Then contact your credit card company, as it may take some time before your refund is officially posted. Next, contact your bank. There is often some processing time before a refund is posted. If you’ve done all of this and you still have not received your refund, please contact us at support@eshopsy.netlify.app.
+<br />
+<br />
+<h1 className='text-2xl font-semibold'>Cancellation Policy</h1>
+
+
+Order Cancellations
+You can cancel your order within 24 hours of placing it. To cancel your order, please follow these steps:
+
+Contact Customer Service: Email us at support@eshopsy.netlify.app with your order number and request for cancellation.
+Receive Confirmation: Wait for our customer service team to confirm your cancellation. If your order has not yet been processed or shipped, we will cancel it and issue a full refund.
+<br />
+<br />
+<h1 className='text-2xl font-semibold'>Orders Already Shipped</h1>
+
+
+If your order has already been shipped, it cannot be canceled. In this case, you may return the item once you receive it, following our return process outlined above.
+
+<br />
+<br />
+<h1 className='text-2xl font-semibold'>Refused Deliveries</h1>
+
+If you refuse to accept delivery of an order that has already been shipped, you will be responsible for the original shipping charges, any duties, taxes, and/or customs charges that are incurred on the package, and the cost of returning the package to eShopsy. This amount will be deducted from your merchandise refund.
+
+
+
+<br />
+<br />
+<h1 className='text-2xl font-semibold'>Damaged or Defective Items</h1>
+If you receive an item that is damaged or defective, please contact our customer service team within 7 days of receipt. Provide us with your order number, a description of the damage or defect, and any relevant photos. We will arrange for a replacement or refund as appropriate.
+
+</div>
 </Layout>
   )
 }
